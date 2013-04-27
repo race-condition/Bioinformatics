13dd09ffb0e305a3a303ca87fe0d4cc2282acc5b
CoderYuan <yyt5070@psu.edu>
2013-04-26 23:49:24 -0300

    Create Calculating Protein Mass.py

---
+++ Calculating Protein Mass.py
@@ -0,0 +1,23 @@
+monoisotopic_mass_table = {
+    'A':71.03711,'C':103.00919,'D':115.02694,'E':129.04259,'F':147.06841,
+    'G':57.02146,'H':137.05891,'I':113.08406,'K':128.09496,'L':113.08406,
+    'M':131.04049,'N':114.04293,'P':97.05276,'Q':128.05858,'R':156.10111,
+    'S':87.03203,'T':101.04768,'V':99.06841,'W':186.07931,'Y':163.06333
+}
+
+
+import os
+os.chdir('C:\\Users\\Terry Tang\\Desktop\\Python3.3\\Rosalind_bioinformatics')
+file=open('rosalind_prtm.txt','r')
+protein=file.readline().strip()
+
+
+mass=0
+
+for i in range(len(protein)):
+    mass=mass+monoisotopic_mass_table[(protein[i])]
+
+print(mass)
+        
+        
+        
